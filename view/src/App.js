f74f0f53aecf79ffc2f7bde09c5785584d7aeb6f
davidwedder <davidgarajan1661@gmail.com>
2024-05-16 16:25:39 -0300

    Finalizando FrontEnd

---
+++ view/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import EstilosGlobais from './componentes/EstilosGlobais';
+import BenVindo from './pages/Benvindo';
+
+
+
+function App(){
+  return(
+<div> 
+  <EstilosGlobais/>
+   <BenVindo/>
+</div>
+  )
+}
+
+export default App;
